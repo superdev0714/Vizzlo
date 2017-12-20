b54df851b89cae4121903b9232edcffa33d22090
superdev <super.dev0714@gmail.com>
2017-12-20 13:06:07 +0100

    Task 3

--- testdrive.tsx
+++ testdrive.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Bar, Pie } from 'react-chartjs-2';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import 'chartjs-plugin-datalabels';
 
 export interface TestDriveProps {
+}
+
+export interface TestDriveState {
+  data: object
 }
 
 const data = {
@@ -42,13 +47,56 @@ const options = {
      }
 }
 
-export default class TestDrive extends React.Component<TestDriveProps, {}> {
+export default class TestDrive extends React.Component<TestDriveProps, TestDriveState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        labels: ['Plane ticket', 'Hotel', 'Rental car', 'Dinner', 'Postcards'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: [
+                'rgba(206,79,64,1)',
+                'rgba(91,152,217,1)',
+                'rgba(115,202,116,1)',
+                'rgba(231,195,44,1)',
+                'rgba(141,92,178,1)',
+            ],
+            data: [268, 199, 49, 112, 3]
+          }
+        ],
+      }
+    };
+  }
+
+  onAdd = () => {
+
+  }
+
+  onRemove = () => {
+    
+  }
+
 	render() {
 		return (
-            <div>
-                <Bar data={data} options={options}/>
-                <Pie data={data} />
-            </div>
-        )
+      <div>
+        <button onClick={this.onAdd}>Add</button>
+        <button onClick={this.onRemove}>Remove</button>
+        <Tabs>
+          <TabList>
+            <Tab>Bar Chart</Tab>
+            <Tab>Pie Chart</Tab>
+          </TabList>
+      
+          <TabPanel>
+            <Bar data={this.state.data} options={options} />
+          </TabPanel>
+          <TabPanel>
+            <Pie data={this.state.data} options={options} />
+          </TabPanel>
+        </Tabs>
+      </div>
+    )
 	}
 }
